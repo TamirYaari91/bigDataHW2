da7eb4e916aa30a50dc27f9decfb91e965bdba68
TamirYaariSpot <tamiry@netapp.com>
2022-12-31 19:17:22 +0200

    basic insert to items works, need to add threads and implement the "item" func in order to test

--- hw2-src/src/bigdatacourse/hw2/studentcode/HW2StudentAnswer.java
+++ hw2-src/src/bigdatacourse/hw2/studentcode/HW2StudentAnswer.java
@@ -7,41 +7,54 @@ import java.util.HashSet;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 
 import bigdatacourse.hw2.HW2API;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class HW2StudentAnswer implements HW2API {
 
     // general consts
     public static final String NOT_AVAILABLE_VALUE = "na";
-    public static final String PATH_TO_ITEMS       = "/Users/tamiry/Desktop/bigDataHW2/data/meta_Office_Products.json";
-	public static final String PATH_TO_REVIEWS = "/Users/tamiry/Desktop/bigDataHW2/data/reviews_Office_Products.json";
+    public static final String ITEM_DOES_NOT_EXIST = "not exists";
+    public static final int    NUM_OF_THREADS      = 100; // TODO - change to 250?
+
+    public static final String PATH_TO_ITEMS   = "/Users/tamiry/Desktop/bigDataHW2/data/meta_Office_Products.json";
+    // TODO - delete when done
+    public static final String PATH_TO_REVIEWS = "/Users/tamiry/Desktop/bigDataHW2/data/reviews_Office_Products.json";
+    // TODO - delet when done
 
     // CQL stuff
-	private static final String		TABLE_ITEMS = "items";
-	private static final String		TABLE_USER_REVIEWS = "user_reviews";
+    public static final String       TABLE_ITEMS      = "items";
+    public static final List<String> TABLE_ITEMS_KEYS =
+            Arrays.asList("asin", "title", "image", "categories", "description");
+
+    public static final String TABLE_USER_REVIEWS = "user_reviews";
+    public static final String TABLE_ITEM_REVIEWS = "item_reviews";
 
 
-	private static final String		CQL_CREATE_TABLE =
-			"CREATE TABLE " + TABLE_USER_VIEW 	+"(" 		+
-			"user_id bigint,"			+
-			"ts timestamp,"				+
-			"video_id bigint,"			+
-			"device text,"				+
-			"duration int,"				+
-			"PRIMARY KEY ((user_id), ts, video_id)"	+
-			") "						+
-			"WITH CLUSTERING ORDER BY (ts DESC, video_id ASC)";
+    public static final String CQL_ITEMS_INSERT =
+            "INSERT INTO " + TABLE_ITEMS + "(asin, title, image, categories, description) VALUES(?, ?, ?, ?, ?)";
+
+    public static final String CQL_ITEMS_SELECT = "SELECT * FROM " + TABLE_ITEMS + " WHERE asin = ?";
 
 
-	// cassandra session
+    public static final String CQL_CREATE_TABLE_ITEMS =
+            "CREATE TABLE " + TABLE_ITEMS + "(" + "asin text," + "title text," + "image text," + "categories text," +
+            "description text," + "PRIMARY KEY (asin)" + ") ";
+
+    // cassandra session
     private CqlSession session;
 
+
     // prepared statements
-    //TODO: add here prepared statements variables
+    private PreparedStatement pstmtAddToItems;
+    private PreparedStatement pstmtSelectFromItems;
 
 
     @Override
@@ -75,20 +88,45 @@ public class HW2StudentAnswer implements HW2API {
 
     @Override
     public void createTables() {
-        //TODO: implement this function
-        System.out.println("TODO: implement this function...");
+        session.execute(CQL_CREATE_TABLE_ITEMS);
+        System.out.println("created table: " + TABLE_ITEMS);
     }
 
     @Override
     public void initialize() {
-        //TODO: implement this function
-        System.out.println("TODO: implement this function...");
+        pstmtAddToItems = session.prepare(CQL_ITEMS_INSERT);
+        pstmtSelectFromItems = session.prepare(CQL_ITEMS_SELECT);
     }
 
     @Override
     public void loadItems(String pathItemsFile) throws Exception {
-        //TODO: implement this function
-        System.out.println("TODO: implement this function...");
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader(pathItemsFile));
+            String     itemString = reader.readLine();
+            JSONObject itemJsonObject;
+
+            while (itemString != null) {
+                itemJsonObject = new JSONObject(itemString);
+                fillItemsJson(itemJsonObject);
+                BoundStatement bstmtAddToItems = pstmtAddToItems.bind().setString(0, itemJsonObject.getString("asin"))
+                                                                .setString(1, itemJsonObject.getString("title"))
+                                                                .setString(2, itemJsonObject.getString("image"))
+                                                                .setString(3, itemJsonObject.getString("categories"))
+                                                                .setString(4, itemJsonObject.getString("description"));
+                session.execute(bstmtAddToItems);
+
+                // read next line
+                itemString = reader.readLine();
+                break; // in order to read only first line since this is a test
+            }
+
+            reader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -162,30 +200,44 @@ public class HW2StudentAnswer implements HW2API {
         System.out.println("total reviews: " + 3);
     }
 
+    private static void fillItemsJson(JSONObject jsonObject) {
+        for (String key : TABLE_ITEMS_KEYS) {
+            try {
+                Object value = jsonObject.get(key);
+                jsonObject.put(key, value.toString());
+            }
+            catch (JSONException e) {
+                jsonObject.put(key, NOT_AVAILABLE_VALUE);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-		BufferedReader reader;
-
-		try {
-			reader = new BufferedReader(new FileReader(PATH_TO_ITEMS));
-			String itemString = reader.readLine();
-			JSONObject itemJsonObject;
-
-			while (itemString != null) {
-				itemJsonObject = new JSONObject(itemString);
-				System.out.println(itemString);
-				System.out.println(itemJsonObject);
-				System.out.println(itemJsonObject.getString("asin"));
-				// read next line
-				itemString = reader.readLine();
-				break; // in order to read only first line since this is a test
-			}
-
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        //        BufferedReader reader;
+        //
+        //        try {
+        //            reader = new BufferedReader(new FileReader(PATH_TO_ITEMS));
+        //            String     itemString = reader.readLine();
+        //            JSONObject itemJsonObject;
+        //
+        //            while (itemString != null) {
+        //                itemJsonObject = new JSONObject(itemString);
+        //                fillJsonIfNull(itemJsonObject);
+        //                System.out.println(itemString);
+        //                System.out.println(itemJsonObject);
+        //                System.out.println(itemJsonObject.getString("asin"));
+        //                // read next line
+        //                itemString = reader.readLine();
+        //                break; // in order to read only first line since this is a test
+        //            }
+        //
+        //            reader.close();
+        //        }
+        //        catch (IOException e) {
+        //            e.printStackTrace();
+        //        }
+    }
 
 
 }
